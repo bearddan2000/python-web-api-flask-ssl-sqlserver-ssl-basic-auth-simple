9eb44cce8a213f001de06bcafa6f0690ae473518
bearddan2000 <bearddan2000@yahoo.com>
2023-07-23 16:21:14 -0400

    initial commit

---
+++ py-srv/bin/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pymssql://sa:z!x<?oB1ab@db/master"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class DogModel(db.Model):
+    __tablename__ = 'dog'
+
+    id = db.Column(db.Integer, primary_key=True)
+    breed = db.Column(db.String())
+    color = db.Column(db.String())
+
+    def __init__(self, breed, color):
+        self.breed = breed
+        self.color = color
+
+users = {
+    'user': 'pass'
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return True
+    return False
+
+@auth.login_required
+@app.route('/dog')
+def handle_beverage():
+    beverages = DogModel.query.all()
+    results = [
+        {
+            "breed": dog.breed,
+            "color": dog.color
+        } for dog in beverages]
+
+    return {"count": len(results), "dog": results, "message": "success"}
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0', port = 5000, debug = True, ssl_contex='adhoc')
